6f0e7e963ffc71805042290483846c9a1a2e58dd
david reilly <davidreilly02@gmail.com>
2023-09-29 07:41:19 +0700

    pulled and updated code

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ print(b)
 print("lol")
 
 
+<<<<<<< Updated upstream
 print('lololololol')
 
 
 
+=======
+print('ooooo')
+
+
+#i made changes then pulled from git. Now how do i resolve this? How do i resolve if someone already made changes
+>>>>>>> Stashed changes
